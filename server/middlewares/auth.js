93ad138c49276a3aab149aa88ddba375812549a2
Aftab Alam <aftabalamdlm@gmail.com>
2025-03-22 00:32:47 +0530

    fixing minor issues

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -4,6 +4,7 @@ const User = require("../models/user");
 
 const isAuthenticated = TryCatch(async (req, res, next) => {
   const token = req.cookies["vib-token"];
+  console.log(req.cookies);
   console.log(token);
   if (!token) {
     return new ErrorHandler(401, `You need to login to access this route`);
